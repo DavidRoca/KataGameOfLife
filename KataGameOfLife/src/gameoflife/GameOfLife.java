69bc958ab0858b9890fc8e328e33b93077742ec6
DavidRoca <drocafab7@alumnes.ub.edu>
2015-10-06 02:05:42 +0200

    Creado constructor y check de celdas vecinas 'vivas'

--- KataGameOfLife/src/gameoflife/GameOfLife.java
+++ KataGameOfLife/src/gameoflife/GameOfLife.java
@@ -1,5 +1,62 @@
 package gameoflife;
 
+import java.util.Arrays;
+
 public class GameOfLife {
+
+	private boolean[][] tablero;
+	
+	/**
+	 * Constructor. Crea un tablero finito de tipo booleano
+	 * dadas las dimensiones pasadas como parámetros y lo inicializa a 'false'.
+	 * 
+	 * @param filas 
+	 * @param columnas
+	 */
+	public GameOfLife(int filas, int columnas){
+		
+		tablero = new boolean[filas][columnas];
+		
+		for(boolean[] b:tablero){
+			Arrays.fill(b,false);
+		}
+		
+	}
+	
+	 /**
+	 * Dada una celda, devuelve el número de celdas adyacentes 'vivas'.
+	 * 
+	 * @param row
+	 * @param column
+	 * @return numero de celdas 'vivas' adyacentes 
+	 */
+	public int cuentaVecinos(int fila, int col) {
+		int contador = 0;
+		
+		for (int x = fila - 1; x <= fila + 1; x++) {
+		    for (int y = col - 1; y <= col + 1; y++) {
+		        if (isAlive(x, y) && (fila != x || col != y)) {
+		            contador++;
+		        }
+		    }
+		}
+		
+		return contador;
+	 }
+
+	/**
+	 * Método que devuelve si una celda se encuentra dentro
+	 * de los límites del tablero y está 'viva'.
+	 * 
+	 * @param fila
+	 * @param col
+	 * @return
+	 */
+	private boolean isAlive(int fila, int col) {
+		
+		return fila >= 0 && col >= 0 && fila < tablero.length && col < tablero[0].length && tablero[fila][col];
+	}
+	
+
 
 }
